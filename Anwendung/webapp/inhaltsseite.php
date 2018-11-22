a6d4bc70c025d58b8066a7360a764e8b42c81350
FairyNatza <lars.tenbrock@gmail.com>
2018-11-22 08:28:47 +0100

    sachen gemacht

--- Anwendung/webapp/inhaltsseite.php
+++ Anwendung/webapp/inhaltsseite.php
@@ -8,10 +8,23 @@ writeLogin();
 writeHeadEnd();
 
 $settings = new clsSettings();
+switch($settings->action) {
+    case "Übernehmen":
+        echo 'Übernehmen';
+        break;
+    case "PDF Drucken":
+        echo 'PDF Drucken';
+        break;
+    case "":
+        echo 'Nothing';
+    default:
+        echo 'Error: Not Implementet';
+        break;
+}
 if($settings->protokollId !== 0) {
     /* highlight_string("<?php\n\getProtokoll($settings->protokollId) =\n" . var_export(getProtokoll($settings->protokollId), true) . ";\n?>"); */
     writeProtokollForm(getProtokoll($settings->protokollId), false);
-    
+
 } else {
     //TODO Neues Protokoll
 }
